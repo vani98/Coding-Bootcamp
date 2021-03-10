5c007f33aeb88d212dab9595506001bfa91a20fa
vanina <carmen_vanina@hotmail.com>
2021-03-09 22:56:51 -0300

    Clase 13, inicio de JS

---
+++ Clase13/index.js
@@ -0,0 +1,51 @@
+var alumnos = ['Alumno1','Alumno2','Alumno3','Alumno4']; 
+
+//loop for tradicional
+for (let i = 0; i < alumnos.length; i++) {
+    console.log(alumnos[i]);
+}
+console.log("=================================== /n");
+//loop for of
+for (const alumno of alumnos) {
+    console.log(alumno);
+}
+
+console.log("=================================== /n");
+let miObjeto = {
+    nombre: "objeto",
+    tipo: "objeto"
+}; 
+
+//loop for in
+for (let propiedad in miObjeto) {
+   console.log(`${propiedad}: ${miObjeto[propiedad]}`);
+}
+
+// Functions ___________________________*
+
+// nucbaTeSaluda("Vani"); // puede ir en cualquier lado 1 - js en fuction tiene top level hoisting, si declaras una funcion termina arriba del código para que sea accesible en todo el scope
+
+//palabra reservada, nombre de funcion y (){}
+function nucbaTeSaluda (nombre){
+    console.log(`${nombre} Nucba te saluda`);
+}
+nucbaTeSaluda("Vani"); // puede ir en cualquier lado 2
+
+// Arrow functrion
+    //Esta utiliza el this de manera distinta
+
+// ¿Cómo agregamos dinamismo? reutilizacion de códigos en multiplicaciones
+
+const multipicarBy = (number) => {
+    let x = number;
+    return function (numeroDespues){
+        return x * numeroDespues;
+    };
+};
+
+// Todo esto son closures, guarda number en el scope y espera por otro valor
+    const multipicarPor5 = multipicarBy(5);
+    console.log(multipicarPor5(5));
+
+    const multipicarPor10 = multipicarBy(10);
+    console.log(multipicarPor10(10));

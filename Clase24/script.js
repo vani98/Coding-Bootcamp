e2381f9f631957d6b162624c013bf4198a05d9cc
vanina <vanii.cva98@gmail.com>
2022-01-03 20:31:37 -0300

    clase 24 y 26'

--- Clase24/script.js
+++ Clase24/script.js
@@ -1,6 +1,4 @@
-// Convertir un objeto en un array
-
-//Forma uno
+//title: CONVERTIR UN OBJETO EN UN ARRAY
 
 const obj = {
   prop1: "uno",
@@ -12,27 +10,46 @@ const obj2 = {
   prop3: "tres",
 };
 
+//? FORMA 1 (con Object.keys y .map)
+
+// Object.keys devuelve el nombre de las llaves de los objetos ---> [ 'prop1', 'prop2', 'prop3' ]
 const objToArray = Object.keys(obj2).map((key) => obj2[key]);
-console.log(objToArray);
+// console.log(objToArray); // [ 'uno', 'dos', 'tres' ]
+
+//? FORMA 2 (con for in)
 
 const array = [];
 
 for (const key in obj2) {
-  console.log(key);
+  // console.log(key);  // hace lo mismo que Object.keys
   array.push(obj2[key]);
 }
-console.log(array);
+// console.log(array);
 
-//Convertir un string en array
+//title CONVERTIR UN STRING(o un iterable) EN ARRAY
+
+//? FORMA 1 (con Array.from)
 
 const string = "hola";
-//si lo que tengo es un iterable lo transformo a array
+//lo que hace es: si lo que tengo es un iterable lo transformo a array
 const arrToString = Array.from(string);
 
-//El for of solo sirve con iterables, con objetos no para eso está el forin
-for (const str of string) {
-  console.log(str);
-}
+/*El for of solo sirve con iterables, con objetos no, para eso está el for in
+Como el string es iterable podemos usarlo en un for of y mostrarlo letra por letra, pero no por eso es un array.*/
+
+// // for (const str of string) {
+// // console.log(str);
+// // }
+
 console.log(arrToString);
 
-//01:05:00
+const lis = document.querySelectorAll("li");
+// console.dir(lis); //Vemos que los li no son arrays, pero si iterables
+const arrayLi = Array.from(lis);
+// console.dir(arrayLi);
+
+//? FORMA 2 (con un spread operator)
+
+//Puedo hacerlo con spread operator porque este tambien opera con iterables
+const arrayLiSpread = [...lis];
+console.dir(arrayLiSpread);
